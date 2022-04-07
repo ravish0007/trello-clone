3980224efe82a0f8565fbfaf0b71c86f25438362
ravish0007 <ravish.nfs@gmail.com>
2022-04-07 23:51:14 +0530

    Update Board.js: Implement removelist and pass it on to list

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,12 +7,20 @@ export default function Board () {
   const [lists, setList] = useState([])
 
   function addList (listname) {
-    setList((lists) => [...lists, listname])
+    const list = {
+      name: listname,
+      id: Date.now()
+    }
+    setList((lists) => [...lists, list])
+  }
+
+  function removeList (id) {
+    setList((lists) => lists.filter(list => list.id !== id))
   }
 
   return (
     <div className='flex overflow-x-auto space-x-4 '>
-      {lists.map(list => <List list={list} />)}
+      {lists.map(list => <List list={list} key={list.id} removeList={removeList} />)}
       <ListInput addList={addList} />
     </div>
   )
