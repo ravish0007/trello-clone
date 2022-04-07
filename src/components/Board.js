ac9ef4dba92681441694a5524985e766fac49e59
ravish0007 <ravish.nfs@gmail.com>
2022-04-07 19:47:14 +0530

    Add state to Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,15 +1,19 @@
+import { useState } from 'react'
 
 import List from './List'
 import ListInput from './ListInput'
 
 export default function Board () {
+  const [lists, setList] = useState([])
+
+  function addList (listname) {
+    setList((lists) => [...lists, listname])
+  }
+
   return (
-    // <div className='flex flex-row overflow-x-auto space-x-4 '>
-    <div className='flex flex-row overflow-x-auto space-x-4 '>
-      <List />
-      <List />
-      <List />
-      <ListInput />
+    <div className='flex overflow-x-auto space-x-4 '>
+      {lists.map(list => <List list={list} />)}
+      <ListInput addList={addList} />
     </div>
   )
 }
