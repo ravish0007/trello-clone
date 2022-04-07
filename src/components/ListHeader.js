ba1f554354560e5a61788fb27c475a0a7e228e1b
ravish0007 <ravish.nfs@gmail.com>
2022-04-08 01:01:27 +0530

    Update ListHeader.js: select text when clicked

--- src/components/ListHeader.js
+++ src/components/ListHeader.js
@@ -14,6 +14,7 @@ export default function ListHeader ({ name, changeListName }) {
         // className='p-2 w-full text-black border-2 border-transparent rounded-sm focus:border-sky-600 focus:outline-none focus:rounded-sm'
         className='basis-3/4 p-2 font-[550] w-full text-black rounded-sm border-2 border-transparent focus:border-sky-600 focus:outline-none focus:rounded-sm'
         autoFocus
+        onFocus={(e) => e.target.select()}
         onKeyUp={(e) => {
           if (e.keyCode === 13) {
             changeListName(listname)
@@ -26,7 +27,7 @@ export default function ListHeader ({ name, changeListName }) {
 
   return (
     <p
-      className='basis-3/4 font-[550] font-sans border-2 border-transparent p-2 inline'
+      className='cursor-pointer basis-3/4 font-[550] font-sans border-2 border-transparent p-2 inline'
       onClick={() => setEdit(true)}
     > {name}
     </p>
