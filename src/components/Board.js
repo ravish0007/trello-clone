c60cdfa17430ccbe1f2f0774b4416e09c2a36e83
ravish0007 <ravish.nfs@gmail.com>
2022-04-14 20:16:24 +0530

    Add dragdrop context

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+
 import List from './List'
 import ListInput from './ListInput'
 
@@ -9,20 +11,56 @@ export default function Board () {
   function addList (listname) {
     const list = {
       name: listname,
-      id: Date.now(),
+      id: String(Date.now()),
       cards: []
     }
+
     setList((lists) => [...lists, list])
   }
 
+  function handleDragEnd (result) {
+    console.log(result)
+
+    const { source, destination } = result
+
+    if (!destination) return
+
+    const [sourceCard] = lists.filter(list => list.id === source.droppableId)[0].cards.splice(source.index, 1)
+
+    setList(lists => {
+      return lists.map(list => list.id !== source.droppableId
+        ? list
+        : { ...list, cards: list.cards.filter(card => card.id !== sourceCard.id) }
+      )
+    })
+
+    const destinationCards = Array.from(lists.filter(list => list.id === destination.droppableId)[0].cards)
+
+    destinationCards.splice(destination.index, 0, sourceCard)
+
+    setList(lists => {
+      return lists.map(list => list.id !== destination.droppableId
+        ? list
+        : { ...list, cards: destinationCards }
+      )
+    })
+  }
+
   return (
-    <div className='flex overflow-x-auto space-x-4 '>
-      {lists.map(list => <List
-        list={list}
-        setList={setList}
-        key={list.id}
-                         />)}
-      <ListInput addList={addList} />
+    <div
+      className='flex overflow-x-auto space-x-4'
+    >
+      <DragDropContext onDragEnd={handleDragEnd}>
+        {lists.map((list, index) =>
+          <List
+            list={list}
+            setList={setList}
+            key={list.id}
+            index={index}
+          />
+        )}
+        <ListInput addList={addList} />
+      </DragDropContext>
     </div>
   )
 }
