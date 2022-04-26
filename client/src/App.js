767d02941877a04b6ee7b3208c3538c4c63f35a6
ravish0007 <ravish.nfs@gmail.com>
2022-04-26 20:21:38 +0530

    Add login state to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,41 @@
 
+import { useState } from 'react'
+
 import Workspace from './components/Workspace.js'
+import Login from './components/Login'
+// import Register from './components/Register'
+
+import trelloService from './trelloService'
 
 export default function App () {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [username, setUsername] = useState('')
+  const [board, setBoard] = useState(null)
+
+  function logOut () {
+    setIsLoggedIn(false)
+    setUsername('')
+    setBoard(null)
+  }
+
+  if (!isLoggedIn) {
+    return (
+      <Login
+        setStatus={setIsLoggedIn}
+        setBoard={setBoard}
+        setUser={setUsername}
+      />
+    )
+  }
+
   return (
     <div>
-      <Workspace />
+      <Workspace
+        username={username}
+        board={board}
+        onClick={() => setIsLoggedIn(false)}
+        logOut={logOut}
+      />
     </div>
   )
 }
