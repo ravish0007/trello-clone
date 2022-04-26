798a40889287b9414db1ec10a6a5d646241920fa
ravish0007 <ravish.nfs@gmail.com>
2022-04-26 20:24:05 +0530

    Pass login state to child components

--- client/src/components/Workspace.js
+++ client/src/components/Workspace.js
@@ -1,10 +1,11 @@
-
 import Board from './Board'
+import Topbar from './Topbar'
 
-export default function Workspace () {
+export default function Workspace ({ board, username, logOut }) {
   return (
-    <div className='min-h-screen p-6 bg-sky-600'>
-      <Board />
+    <div className='min-h-screen bg-sky-600'>
+      <Topbar username={username} logOut={logOut} />
+      <Board board={board} />
     </div>
   )
 }
