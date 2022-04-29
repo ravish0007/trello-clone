0cbce577a531d119c63870ebcd36e7af989117e0
ravish0007 <ravish.nfs@gmail.com>
2022-04-29 06:59:43 +0530

    Update Workspace.js: reflect state change

--- client/src/components/Workspace.js
+++ client/src/components/Workspace.js
@@ -1,10 +1,10 @@
 import Board from './Board'
 import Topbar from './Topbar'
 
-export default function Workspace ({ board, username, logOut }) {
+export default function Workspace ({ board, user, logOut }) {
   return (
     <div className='min-h-screen bg-sky-600'>
-      <Topbar username={username} logOut={logOut} />
+      <Topbar user={user} logOut={logOut} />
       <Board board={board} />
     </div>
   )
