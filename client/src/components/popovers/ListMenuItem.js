ee115fff2ddbcbfb9ed5f0455f868ac4c2ef21b2
ravish0007 <ravish.nfs@gmail.com>
2022-04-29 07:00:41 +0530

    Update ListMenuItem.js: center menu items

--- client/src/components/popovers/ListMenuItem.js
+++ client/src/components/popovers/ListMenuItem.js
@@ -2,7 +2,7 @@
 export default function ListMenuItem ({ name, handleClick }) {
   return (
     <p
-      className='py-2 px-4 text-slate-700 rounded-sm hover:bg-gray-200'
+      className='text-center py-2 px-4 text-slate-700 rounded-sm hover:bg-gray-200'
       onClick={handleClick}
     >
       {name}
