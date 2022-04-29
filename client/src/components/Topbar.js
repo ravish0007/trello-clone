da9385150811bc203faa7d7a674d4fc6f11467fa
ravish0007 <ravish.nfs@gmail.com>
2022-04-29 06:56:59 +0530

    Update Topbar.js: reflect state change

--- client/src/components/Topbar.js
+++ client/src/components/Topbar.js
@@ -2,13 +2,13 @@ import { useState } from 'react'
 
 import UserPopover from './popovers/UserPopover'
 
-export default function Topbar ({ username, logOut }) {
+export default function Topbar ({ user, logOut }) {
   const [showMenu, setShowMenu] = useState(false)
   return (
 
     <div className='p-3 bg-sky-700 relative'>
       <UserPopover
-        username={username}
+        user={user}
         open={showMenu}
         logOut={logOut}
         closeMenu={() => setShowMenu(false)}
@@ -16,11 +16,14 @@ export default function Topbar ({ username, logOut }) {
 
       <div className='flex justify-between'>
         <span className='text-white text-2xl italic'> Trello </span>
+
         <span
           className='cursor-pointer h-10 w-10 p-2 rounded-full text-red-900 bg-gray-200 text-center text-lg'
-          onClick={() => setShowMenu(true)}
-        >{username[0].toUpperCase()}
+          onClick={(e) => setShowMenu(true)}
+        >
+          {user.name[0].toUpperCase()}
         </span>
+
       </div>
     </div>
   )
