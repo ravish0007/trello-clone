7befed92a45d63ef6d28251bdaa8f3b2c36334fe
ravish0007 <ravish.nfs@gmail.com>
2022-04-29 06:44:31 +0530

    Refactor and Update server.js: add cookie parser and remove comments

--- server/server.js
+++ server/server.js
@@ -1,60 +1,31 @@
 const express = require('express')
 const cors = require('cors')
 
-const morgan = require('morgan')
+// const morgan = require('morgan')
 
 const router = require('./routes')
 
 const cookieSession = require('cookie-session')
+const cookieParser = require('cookie-parser')
 
 const verifyToken = require('./verifyToken')
 
 const app = express()
 
-app.use(morgan('combined'))
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+// app.use(morgan('combined'))
 
-// app.use(cors(
-//   {
-//     origin: 'http://localhost:3000',
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-//   }
-// ))
+app.use(cors({ origin: process.env.UI_ROOT, credentials: true }))
 
-// app.use(function (req, res, next) {
-//   res.header('Content-Type', 'application/json;charset=UTF-8')
-//   res.header('Access-Control-Allow-Credentials', true)
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   )
-//   next()
-// })
+app.use(cookieSession({ secret: process.env.SESSION_SECRET, httpOnly: true, sameSite: 'strict' }))
+app.use(cookieParser())
 
 app.use(express.json())
-// app.use(express.static('public'))
-app.use(cookieSession({ secret: process.env.SESSION_SECRET, httpOnly: true }))
+app.use(express.static('public'))
 
 app.use('/api/lists', verifyToken, router.trelloListRoute)
 app.use('/api/cards', verifyToken, router.trelloCardRoute)
 app.use('/api/auth', router.authRoute)
 
 const PORT = process.env.SERVER_PORT || 3001
-
-// const https = require('https')
-// const fs = require('fs')
-// https
-//   .createServer(
-//     {
-//       key: fs.readFileSync('server.key'),
-//       cert: fs.readFileSync('server.cert')
-//     },
-//     app
-//   )
-//   .listen(PORT, function () {
-//     console.log(
-//       'Example app listening on port 3000! Go to https://localhost:3000/'
-//     )
-//   })
 
 app.listen(PORT, () => { console.log(`Server running.. on ${PORT}`) })
