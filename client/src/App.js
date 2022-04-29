a8491c43e1876093374d27525d2dcb8b19cbeaff
ravish0007 <ravish.nfs@gmail.com>
2022-04-29 06:41:50 +0530

    Update App.js: refactor state and cookie based data fetch

--- client/src/App.js
+++ client/src/App.js
@@ -4,49 +4,60 @@ import { useState, useEffect } from 'react'
 import Workspace from './components/Workspace.js'
 import Login from './components/Login'
 
+import TrelloService from './trelloService.js'
 import { removeJwtToken, getJwtToken } from './tokenManager'
 
 export default function App () {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [username, setUsername] = useState('')
+  const [user, setUser] = useState('')
   const [board, setBoard] = useState(null)
 
+  // useEffect(() => {
+  //   if (getJwtToken()) {
+  //     setBoard(JSON.parse(sessionStorage.getItem('board')))
+  //     setUsername(JSON.parse(sessionStorage.getItem('username')))
+  //     setIsLoggedIn(true)
+  //   }
+  // }, [])
+
   useEffect(() => {
-    if (getJwtToken()) {
-      setBoard(JSON.parse(sessionStorage.getItem('board')))
-      setUsername(JSON.parse(sessionStorage.getItem('username')))
-      setIsLoggedIn(true)
+    if (document.cookie.includes('googleOauth')) {
+      TrelloService.googleUser().then(data => {
+        setUser(data.user)
+        setBoard(data.board)
+        setIsLoggedIn(true)
+      })
     }
   }, [])
 
   async function logOut () {
-    removeJwtToken()
+    TrelloService.logoutUser().then(() => {
+      setIsLoggedIn(false)
+      setUser('')
+      setBoard(null)
+    })
 
-    setIsLoggedIn(false)
-    setUsername('')
-    setBoard(null)
-
-    sessionStorage.removeItem('board')
-    sessionStorage.removeItem('username')
+    // sessionStorage.removeItem('board')
+    // sessionStorage.removeItem('username')
   }
 
   if (!isLoggedIn) {
     return (
       <Login
-        setStatus={setIsLoggedIn}
+        setLoginStatus={setIsLoggedIn}
         setBoard={setBoard}
-        setUser={setUsername}
+        setUser={setUser}
       />
     )
   }
 
-  sessionStorage.setItem('board', JSON.stringify(board))
-  sessionStorage.setItem('username', JSON.stringify(username))
+  // sessionStorage.setItem('board', JSON.stringify(board))
+  // sessionStorage.setItem('username', JSON.stringify(username))
 
   return (
     <div>
       <Workspace
-        username={username}
+        user={user}
         board={board}
         onClick={() => setIsLoggedIn(false)}
         logOut={logOut}
